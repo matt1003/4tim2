b38cba4c1cfa402a41986fd39493ae2504d750df
rmelch <robert.melchers@gmail.com>
2017-01-17 23:02:34 +1300

    New setup.py file.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='elmg_configtool',
+    version='0.1',
+    packages=['', 'RegisterTool'],
+    url='http://www.elmgdigitalpower.com/',
+    license='',
+    author='Elmg',
+    author_email='',
+    description='Configuration tool'
+)
